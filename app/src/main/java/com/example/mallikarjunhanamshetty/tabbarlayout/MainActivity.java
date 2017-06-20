beae4e290be3c3ac43e7c1499cf127b8c4470324
mjidaga@gmail.com <mjidaga@gmail.com>
2017-06-20 22:01:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mallikarjunhanamshetty/tabbarlayout/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.mallikarjunhanamshetty.tabbarlayout;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        tabLayout = (TabLayout)findViewById(R.id.tabLayout);
+        viewPager =(ViewPager)findViewById(R.id.viewpager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter .addFragments(new HomeFragment(),"Home");
+        viewPagerAdapter .addFragments(new FreeFragment(),"free");
+        viewPagerAdapter .addFragments(new TopPaidFragment(),"Paid");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/mallikarjunhanamshetty/tabbarlayout/ViewPagerAdapter.java
@@ -0,0 +1,47 @@
+package com.example.mallikarjunhanamshetty.tabbarlayout;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.View;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mallikarjunhanamshetty on 6/20/17.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> tabtitles = new ArrayList<>();
+
+
+    public void addFragments(Fragment fragment,String title)
+    {
+        fragments.add(fragment);
+        tabtitles.add(title);
+    }
+
+    public ViewPagerAdapter(FragmentManager fm)
+    {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return tabtitles.get(position);
+    }
+}
